55dc0f81099f5946dd1d8623252370d067850479
Дмитро Рудненко <dimarudnenko@MacBook-Air-Dmitro.local>
2024-04-04 16:57:11 +0200

    lastest updatet

--- src/api/http/index.ts
+++ src/api/http/index.ts
@@ -1,7 +1,7 @@
 import AuthService from 'api/services/AuthService';
 import axios from 'axios';
 
-export const API_URL = 'http://localhost:4000/api';
+export const API_URL = 'https://deploy-mern-api-pi.vercel.app';
 
 const $api = axios.create({
     withCredentials: true,
