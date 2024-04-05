ba99ca838990001fe481608ec4f23738f0e63d81
Дмитро Рудненко <dimarudnenko@MacBook-Air-Dmitro.local>
2024-04-06 00:16:07 +0200

    updates

--- src/api/http/index.ts
+++ src/api/http/index.ts
@@ -1,7 +1,7 @@
 import AuthService from 'api/services/AuthService';
 import axios from 'axios';
 
-export const API_URL = 'https://helpful-costume-fawn.cyclic.app';
+export const API_URL = 'https://helpful-costume-fawn.cyclic.app/api';
 
 const $api = axios.create({
     withCredentials: true,
